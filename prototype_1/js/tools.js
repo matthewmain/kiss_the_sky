458efdf760cc3cd023e56f97a71e02eebba72e25
matthewmain <matthewwmain@gmail.com>
2018-11-20 20:52:00 -0500

    updates tool.js object name

--- prototype_1/js/tools.js
+++ prototype_1/js/tools.js
@@ -9,12 +9,12 @@ const Tool = {
 
 	//random integer between two numbers
 	rib: function( min, max ) {
-  	return Math.floor( Math.random() * ( max - min + 1 ) ) + min;
+ 		return Math.floor( Math.random() * ( max - min + 1 ) ) + min;
 	},
 
 	//random float between two numbers
 	rfb: function( min, max ) {
-  	return Math.random() * ( max - min ) + min;
+ 		return Math.random() * ( max - min ) + min;
 	}
 
 
