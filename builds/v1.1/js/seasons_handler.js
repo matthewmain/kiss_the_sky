794c8f052063795f87bdf34bb618d031c41b7042
matthewmain <matthewwmain@gmail.com>
2019-02-25 12:20:52 -0500

    begins game with full spring background

--- builds/v1.1/js/seasons_handler.js
+++ builds/v1.1/js/seasons_handler.js
@@ -46,7 +46,7 @@ var BgG = {
 
 ///current and previous season background color stop collection objects
 var csbg = BgG.sp;  // current season background
-var psbg = BgG.wi;  // previous season background
+var psbg = BgG.sp;  // previous season background
 
 ///current color stop rgba objects
 var ccs1 = psbg.cs1;  // current color stop 1
@@ -123,17 +123,15 @@ function renderBackground() {
     case "Fall": csbg = BgG.fa; psbg = BgG.su; break;
     case "Winter": csbg = BgG.wi; psbg = BgG.fa; break;
   }
+  if ( currentYear === 1 && currentSeason === "Spring") {
+    csbg = BgG.sp; psbg = BgG.sp;  // starts game off with full spring background, not in mid-transition from winter
+  }
   if ( gameHasBegun ) {
     ccs1 = Tl.rgbaCs( psbg.cs1, csbg.cs1, ccs1, 200/renderFactor );  // current color stop redshift 
     ccs2 = Tl.rgbaCs( psbg.cs2, csbg.cs2, ccs2, 200/renderFactor );  // current color stop greenshift
     ccs3 = Tl.rgbaCs( psbg.cs3, csbg.cs3, ccs3, 200/renderFactor );  // current color stop blueshift
     ccs4 = Tl.rgbaCs( psbg.cs4, csbg.cs4, ccs4, 200/renderFactor );  // current color stop alphashift
-  } else {
-    ccs1 = BgG.sp.cs1;  // spring color stop redshift
-    ccs2 = BgG.sp.cs2;  // spring color stop greenshift
-    ccs3 = BgG.sp.cs3;  // spring color stop blueshift
-    ccs4 = BgG.sp.cs4;  // spring color stop alphashift
-  }
+  } 
   var grd=ctx.createLinearGradient( 0, 0, 0, canvas.height );
   grd.addColorStop( 0,
     "rgba("+ Math.round(ccs1.r)+","+
