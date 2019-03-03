0026e22f23866776ed0b8e27213ed0c3acf5a908
matthewmain <matthewwmain@gmail.com>
2019-03-03 12:15:18 -0500

    fixes footer resize issue

--- builds/v1.1/js/ui.js
+++ builds/v1.1/js/ui.js
@@ -80,7 +80,7 @@ function attachHeaderAndFooter() {
 ///scales text content down for smaller window sizes
 function scaleFooterContent() {
   if ( $("#canvas_container_div").width() < 230 ) {
-    $(".text").css({ fontSize: "4pt", top: "1px" });
+    $(".footer_text").css({ fontSize: "4pt", top: "1px" });
     $("#pie_svg_left").css({ top: "7.4px" });
     $("#tag_content").css({ fontSize: "5pt", top: "12px" });
     $("#pie_svg_right").css({ top: "6.35px" });
@@ -89,7 +89,7 @@ function scaleFooterContent() {
     $("#game_win_gen_number").css({ fontSize: "12pt"});
     $("#game_win_mode").css({ fontSize: "10pt"});
   } else if ( $("#canvas_container_div").width() < 300 ) {
-    $(".text").css({ fontSize: "7pt", top: "3px" });
+    $(".footer_text").css({ fontSize: "7pt", top: "3px" });
     $("#pie_svg_left").css({ top: "7.4px" });
     $("#tag_content").css({ fontSize: "7pt", top: "15.5px" });
     $("#pie_svg_right").css({ top: "8.35px" });
@@ -98,7 +98,7 @@ function scaleFooterContent() {
     $("#game_win_gen_number").css({ fontSize: "14pt"});
     $("#game_win_mode").css({ fontSize: "12pt"});
   } else if ( $("#canvas_container_div").width() < 400 ){ 
-    $(".text").css({ fontSize: "9pt", top: "7px" });
+    $(".footer_text").css({ fontSize: "9pt", top: "7px" });
     $("#pie_svg_left").css({ top: "13.4px" });
     $("#tag_content").css({ fontSize: "8pt", top: "19.5px" });
     $("#pie_svg_right").css({ top: "12.35px" });
@@ -107,7 +107,7 @@ function scaleFooterContent() {
     $("#game_win_gen_number").css({ fontSize: "16pt"});
     $("#game_win_mode").css({ fontSize: "15pt"});
   } else {  // > 400
-    $(".text").css({ fontSize: "9pt", top: "12px" });
+    $(".footer_text").css({ fontSize: "9pt", top: "12px" });
     $("#pie_svg_left").css({ top: "18.4px" });
     $("#tag_content").css({ fontSize: "10pt", top: "24.5px" });
     $("#pie_svg_right").css({ top: "17.35px" });
