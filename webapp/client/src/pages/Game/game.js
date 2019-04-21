5c2ff952e8f56997376e237e08d2e09dd0390e2b
Bob Main <bobmain49@gmail.com>
2019-04-20 19:19:36 -0500

    moved onload make fade to within on mount

--- webapp/client/src/pages/Game/game.js
+++ webapp/client/src/pages/Game/game.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react'
 import "./game.sass"
+import $ from "jquery"
 
 class Home extends Component {
 
   componentDidMount(){
-    console.log('mount')
     const game = document.getElementById("game")
     game.style.display = "block"
     game.style.opacity = 1
+    document.addEventListener("DOMContentLoaded", function() {
+      $("#loader_div").fadeOut(1500)
+    })
+
   }
 
   componentWillUnmount(){
