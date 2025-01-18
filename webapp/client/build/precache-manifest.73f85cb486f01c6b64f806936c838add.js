249b7dcc7bfbf1ca2effd9173dcdac69d462a597
robbobfrh84 <bobmain49@gmail.com>
2025-01-18 10:25:02 -0800

    updating broken sass links that were bracking the site

---
+++ webapp/client/build/precache-manifest.73f85cb486f01c6b64f806936c838add.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dacf1c150a5368897c1ae7a9fa255a31",
+    "url": "/kiss_the_sky/index.html"
+  },
+  {
+    "revision": "052edd8caa5f4f424230",
+    "url": "/kiss_the_sky/static/css/main.ede4422f.chunk.css"
+  },
+  {
+    "revision": "44a159ed69a403b86cb7",
+    "url": "/kiss_the_sky/static/js/2.a61df636.chunk.js"
+  },
+  {
+    "revision": "084b1a9a6969b9283af7d745ffd9330d",
+    "url": "/kiss_the_sky/static/js/2.a61df636.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "052edd8caa5f4f424230",
+    "url": "/kiss_the_sky/static/js/main.e8431713.chunk.js"
+  },
+  {
+    "revision": "36070c4b8db2b14ac837",
+    "url": "/kiss_the_sky/static/js/runtime-main.a27e132b.js"
+  },
+  {
+    "revision": "0d5114241fb2ce95c3217b8445b1b10a",
+    "url": "/kiss_the_sky/static/media/edit_icon.0d511424.svg"
+  },
+  {
+    "revision": "288cc37eb5f7aa75b85408664d635580",
+    "url": "/kiss_the_sky/static/media/icon_check_green.288cc37e.svg"
+  },
+  {
+    "revision": "906a3591707225a934b618e50ac4bc47",
+    "url": "/kiss_the_sky/static/media/icon_exit_modal.906a3591.svg"
+  },
+  {
+    "revision": "fcac82290f12b5c3f8bb93c593ec1f0a",
+    "url": "/kiss_the_sky/static/media/icon_exit_modal_black.fcac8229.svg"
+  },
+  {
+    "revision": "562ce5e177487992b28c2169da5508cf",
+    "url": "/kiss_the_sky/static/media/icon_exit_modal_red.562ce5e1.svg"
+  },
+  {
+    "revision": "ec8e459a504df298e69ba7c7486749f3",
+    "url": "/kiss_the_sky/static/media/icon_loading.ec8e459a.svg"
+  },
+  {
+    "revision": "be660857e32d56e2aa2458dce8e3f944",
+    "url": "/kiss_the_sky/static/media/icon_menu.be660857.svg"
+  },
+  {
+    "revision": "6431d77adde1307c5d7ba935a912f6bd",
+    "url": "/kiss_the_sky/static/media/icon_menu_close.6431d77a.svg"
+  },
+  {
+    "revision": "3553549d4e09e28c5d52e4d8dfd27d22",
+    "url": "/kiss_the_sky/static/media/paginate_arrow.3553549d.svg"
+  },
+  {
+    "revision": "f48696c01bbe71454f3a41935ce63b2f",
+    "url": "/kiss_the_sky/static/media/resume_button.f48696c0.svg"
+  },
+  {
+    "revision": "f48696c01bbe71454f3a41935ce63b2f",
+    "url": "/kiss_the_sky/static/media/start_button.f48696c0.svg"
+  },
+  {
+    "revision": "51aa020b76fc71090c4c31949c1283ef",
+    "url": "/kiss_the_sky/static/media/title_header_dark.51aa020b.png"
+  },
+  {
+    "revision": "0dc8ca49b13584a72bdd463b934e9fac",
+    "url": "/kiss_the_sky/static/media/title_header_light.0dc8ca49.png"
+  }
+]);
